2bdc0d228c1bdf8cbd66f0d0421ca4bca6c9df0e
Govind kumar <134090540+GovindKumar04@users.noreply.github.com>
2025-10-25 21:48:24 +0530

    "quck_sort_correction"

--- Array/sorting/quicksort.cpp
+++ Array/sorting/quicksort.cpp
@@ -1,48 +1,58 @@
 #include <iostream>
 using namespace std;
 
-
-int arr[]={4,2,5,1,3,7,9,87,3645,8,9};
-int partion(int arr[], int low, int high){
-    int pivot= arr[low];
-    int pivotIndex=0;
-    for(int i=0; i<=high; i++){
-        if(pivot>arr[i]){
+int arr[] = {4, 2, 5, 1, 3, 7, 9, 87, 3645, 8, 9};
+int partion(int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int pivotIndex = 0;
+    for (int i = 0; i <= high; i++)
+    {
+        if (pivot > arr[i])
+        {
             pivotIndex++;
         }
     }
-    swap(arr[low],arr[pivotIndex]);
-    int i = low, j=high;
-    while (i<pivotIndex && j>pivotIndex){
-        while(arr[i]<pivot){
+    swap(arr[low], arr[pivotIndex]);
+    int i = low, j = high;
+    while (i < pivotIndex && j > pivotIndex)
+    {
+        while (arr[i] < pivot)
+        {
             i++;
         }
-        while(arr[j]>pivot){
+        while (arr[j] > pivot)
+        {
             j--;
         }
 
-    if(i<pivotIndex && j>pivotIndex){
-        swap(arr[i],arr[j]);
+        if (i < pivotIndex && j > pivotIndex)
+        {
+            swap(arr[i], arr[j]);
+        }
     }
-    }
-    return pivotIndex;   
+    return pivotIndex;
 }
-void quickSort(int arr[], int low, int high) {
-     if (low >= high) {
-        return ;
-         }
+void quickSort(int arr[], int low, int high)
+{
+    if (low >= high)
+    {
+        return;
+    }
     // Partition the array into two subarrays and get the pivot index
     int pivotIndex = partion(arr, low, high);
 
-     // Recursively sort the subarrays
+    // Recursively sort the subarrays
     quickSort(arr, pivotIndex + 1, high);
-    quickSort(arr, low, pivotIndex - 1); 
+    quickSort(arr, low, pivotIndex - 1);
 }
 
-int main(){
+int main()
+{
     int size = sizeof(arr) / sizeof(arr[0]);
-    quickSort(arr,0,size-1);
-    for(int i=0; i<size; i++){
-        cout<<arr[i]<< " "; 
+    quickSort(arr, 0, size - 1);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
     }
 }
--- Array/sorting/quiksortTest.cpp
+++ Array/sorting/quiksortTest.cpp
@@ -1,32 +1,52 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int partion(int arr[], int low, int high){
-    int pivot= arr[low];
-    int pivotIndex= 0;
-
-    for(int i=0; i<high; i++){
-        if(arr[i]<pivot){
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int pivotIndex = low;
+    for (int i = low+1; i <= high; i++)
+    {
+        if (arr[i] < pivot)
             pivotIndex++;
-        }
     }
-    swap(arr[low],arr[pivotIndex]);
-    int i=low,j=high;
-    while(i<pivotIndex&& j>pivotIndex){
-       if(arr[i]<pivot){
-        i++;
-       } 
-       if(arr[j]>pivot){
-        j--;
-       }
-       swap(arr[i],arr[j]);
+    swap(arr[low], arr[pivotIndex]);
+    int i = low;
+    int j = high;
+
+    while (i < pivotIndex && j > pivotIndex)
+    {
+        while (arr[i] < pivot)
+            i++;
+        while (arr[j] > pivot)
+            j--;
+        if (i < pivotIndex && j > pivotIndex)
+        {
+            swap(arr[i++], arr[j--]);
+        }
     }
     return pivotIndex;
 }
+void quickSort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int partion = partition(arr, low, high);
 
-void quickSort(int arr[],int low, int high){
-    if(low>=high){
-        return ;
+    quickSort(arr, low, partion - 1);
+    quickSort(arr, partion + 1, high);
+}
+int main()
+{
+    vector<int> arr = {10, 80, 30, 90, 40, 50, 70};
+
+    int high = arr.size() - 1;
+    quickSort(arr, 0, high);
+    for (int i = 0; i <= high; i++)
+    {
+        cout << arr[i] << " ";
     }
-    int parIm
+    return 0;
 }
